db91085506c1c3ed6bc49c48e2a60ea2a861a0b4
s6411423101 <s6411423101@pkru.ac.th>
2024-06-19 10:21:25 +0700

    Footer ( In Procressing ... )

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
-import { PopupWidget } from "@/components/PopupWidget";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -24,7 +23,6 @@ export default function RootLayout({
           <Navbar />
           <div>{children}</div>
           <Footer />
-          <PopupWidget />
         </ThemeProvider>
       </body>
     </html>
