c7f7c7c20e617947e591e8a93807334d276d15a1
s6411423101 <s6411423101@pkru.ac.th>
2024-06-19 14:01:43 +0700

    Page Text (Update)

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
-export default function Home() {
+import React from "react";
+
+const Home = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "50vh",
-      }}
-    >
-      <div
-        className="snap-center"
-        style={{ textAlign: "center", fontSize: "2em" }}
-      >
-        Hello World PKRU
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="flex items-center justify-center text-4xl sm:text-6xl md:text-8xl lg:text-9xl">
+        IN COMMING .....
       </div>
     </div>
   );
-}
+};
+
+export default Home;
